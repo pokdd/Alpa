ece21e95936fedb3ae90923a7347f0a0118c7a16
alpa <101903606+pokdd@users.noreply.github.com>
2022-09-26 22:52:44 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,6 @@
 local games = {
     ["4951858512"] = "https://raw.githubusercontent.com/pokdd/1/main/Victory%20Race.lua",
     ["7952502098"] = "https://raw.githubusercontent.com/pokdd/1/main/Impossible%20Glass%20Bridge%20Obby.lua",
-    ["7761641014"] = "\39\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\112\111\107\100\100\47\49\47\109\97\105\110\47\84\101\115\116\46\108\117\97\39\10",
 }
 
 for id, url in next, games do
