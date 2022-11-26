5b09fd137838be1744e5c5d065858765502ab7db
alpa <101903606+pokdd@users.noreply.github.com>
2022-11-27 00:17:24 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,6 @@
 local games = {
     ["4951858512"] = "https://raw.githubusercontent.com/pokdd/Alpa/main/Games/Victory%20Race/script.lua",
-    ["7952502098"] = "https://raw.githubusercontent.com/pokdd/1/main/Impossible%20Glass%20Bridge%20Obby.lua",
+    ["7952502098"] = "https://raw.githubusercontent.com/pokdd/Alpa/main/Games/Impossible%20Glass%20Bridge%20Obby%20Squid%20Game/script.lua",
     ["2788229376"] = "https://raw.githubusercontent.com/pokdd/1/main/Da%20Hood.lua",
 }
 
