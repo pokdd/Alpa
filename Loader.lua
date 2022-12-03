b5a0c97a679c651068f93f509fb1f45447cedc85
alpa <101903606+pokdd@users.noreply.github.com>
2022-12-03 16:18:07 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,7 @@
 local games = {
     ["4951858512"] = "https://raw.githubusercontent.com/pokdd/Alpa/main/Games/Victory%20Race/script.lua",
     ["7952502098"] = "https://raw.githubusercontent.com/pokdd/Alpa/main/Games/Impossible%20Glass%20Bridge%20Obby%20Squid%20Game/script.lua",
-    ["2788229376"] = "https://raw.githubusercontent.com/pokdd/1/main/Da%20Hood.lua",
+    ["2788229376"] = "https://raw.githubusercontent.com/pokdd/Alpa/main/Games/Da%20Hood/script",
 }
 
 for id, url in next, games do
